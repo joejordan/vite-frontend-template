54336fd831c2670cf6840bfada0bec8be821644c
Joe Jordan <dev@ignitesoft.com>
2024-06-21 13:15:50 -0400

    fix: rename to fix case sensitivity

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app-temp';
+import App from './app';
 import './app.css';
 
 createRoot(document.querySelector('#root')!).render(
