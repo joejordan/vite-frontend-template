72986fad55315cbcd02182a8e95f4fe1f166eaf1
Joe Jordan <dev@ignitesoft.com>
2024-08-07 17:44:03 -0400

    chore: eslint config tweaks
    re-added tailwindcss after the migration to antfu-eslint-config
    re-added unicorn/recommended eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,44 @@
 import antfu from '@antfu/eslint-config';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+
+// workaround for flat config not being supported yet by eslint-plugin-tailwindcss
+// https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/280
+// how-to from: https://github.com/antfu/eslint-config/issues/431#issuecomment-2014668812
+const customTailwindConfig = new FlatCompat().config({
+  extends: ['plugin:tailwindcss/recommended'],
+  rules: {
+    'tailwindcss/no-custom-classname': 'warn',
+  },
+});
+
+// Add the name property to the config so it displays correctly in the eslint config-inspector (https://github.com/eslint/config-inspector)
+// To run the inspector, use `pnpx @eslint/config-inspector` in the root of the project.
+customTailwindConfig[0].name = 'tailwindcss/recommended';
+
+// customized unicorn config from the antfu eslint config to include recommended rules.
+const customUnicornConfig = {
+  ...eslintPluginUnicorn.configs['flat/recommended'],
+  rules: {
+    ...eslintPluginUnicorn.configs['flat/recommended'].rules,
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/consistent-function-scoping': 'off',
+    // ignore react hook-based filenames, LICENSE, and README
+    'unicorn/filename-case': [
+      'error',
+      {
+        case: 'kebabCase',
+        ignore: [
+          // This regex matches files that start with 'use' followed by an uppercase letter
+          // and then any sequence of characters, ending with '.ts'
+          String.raw`^use[A-Z].*\.ts$`,
+          'LICENSE',
+          'README',
+        ],
+      },
+    ],
+  },
+};
 
 export default antfu({
   ignores: ['dist/', '**/dist/**/', 'coverage/', '**/coverage/**/'],
@@ -13,9 +53,6 @@ export default antfu({
     semi: true,
     trailingComma: 'all',
   },
-  extends: [
-    'plugin:unicorn/recommended',
-  ],
   rules: {
     'style/arrow-parens': ['error', 'always'],
     'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
@@ -81,24 +118,9 @@ export default antfu({
         destructuredArrayIgnorePattern: '^_',
       },
     ],
-    'unicorn/prevent-abbreviations': 'off',
-    'unicorn/consistent-function-scoping': 'off',
-    // ignore react hook-based filenames
-    'unicorn/filename-case': [
-      'error',
-      {
-        case: 'kebabCase',
-        ignore: [
-          // This regex matches files that start with 'use' followed by an uppercase letter
-          // and then any sequence of characters, ending with '.ts'
-          String.raw`^use[A-Z].*\.ts$`,
-          'LICENSE',
-          'README',
-        ],
-      },
-    ],
   },
   parserOptions: {
     project: './tsconfig.eslint.json',
   },
-});
+}, ...customTailwindConfig)
+  .replace('antfu/unicorn/rules', customUnicornConfig);
