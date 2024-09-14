8f5d596e450bfc8187206adae239c58c6d151bc7
Joe Jordan <dev@ignitesoft.com>
2024-09-13 22:52:28 -0400

    feat: add script to rewrite package.json on template creation

---
+++ .github/scripts/rename.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# https://gist.github.com/vncsna/64825d5609c146e80de8b1fd623011ca
+set -euo pipefail
+
+# Define the input vars
+GITHUB_REPOSITORY=${1?Error: Please pass username/repo, e.g. joejordan/vite-frontend-template}
+GITHUB_REPOSITORY_OWNER=${2?Error: Please pass username, e.g. joejordan}
+GITHUB_REPOSITORY_DESCRIPTION=${3:-""} # If null then replace with empty string
+
+echo "GITHUB_REPOSITORY: $GITHUB_REPOSITORY"
+echo "GITHUB_REPOSITORY_OWNER: $GITHUB_REPOSITORY_OWNER"
+echo "GITHUB_REPOSITORY_DESCRIPTION: $GITHUB_REPOSITORY_DESCRIPTION"
+
+# jq is like sed for JSON data
+JQ_OUTPUT=`jq \
+  --arg NAME "@$GITHUB_REPOSITORY" \
+  --arg AUTHOR_NAME "$GITHUB_REPOSITORY_OWNER" \
+  --arg URL "https://github.com/$GITHUB_REPOSITORY_OWNER" \
+  --arg DESCRIPTION "$GITHUB_REPOSITORY_DESCRIPTION" \
+  '.name = $NAME | .description = $DESCRIPTION | .author |= ( .name = $AUTHOR_NAME | .url = $URL )' \
+  package.json
+`
+
+# Overwrite package.json
+echo "$JQ_OUTPUT" > package.json
