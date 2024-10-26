2a33fd515743614c87d14d1318452f7b58afb7e0
Joe Jordan <dev@ignitesoft.com>
2024-10-26 11:55:04 -0400

    refactor: migrate tailwind config to typescript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './*.html'],
   darkMode: 'media',
   mode: 'jit',
 };
+
+export default config;
